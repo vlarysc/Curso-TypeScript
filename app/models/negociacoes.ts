e58d8c6482ef1420976d0c343d12131168394eb2
vlarysc <74836578+vlarysc@users.noreply.github.com>
2022-07-07 11:12:39 -0300

    commit

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,16 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+  private negociacoes: Array<Negociacao> = [];
+  adiciona(negociacao: Negociacao) {
+    this.negociacoes.push(negociacao);
+  }
+  lista(): ReadonlyArray<Negociacao> {
+    return this.negociacoes;
+  }
+}
+
+const negociacoes = new Negociacoes();
+negociacoes.lista().forEach((n) => {
+  n;
+});
